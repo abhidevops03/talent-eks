6429e12f6faf36992ad2086f3eb830b351a94211
abhidevops03 <abhidevops03@gmail.com>
2024-12-18 11:46:56 +0530

    Update backend-s3.tf

--- backend-s3.tf
+++ backend-s3.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "talent-eks-terraform-state-file"
     key    = "terraform.tfstate"  # your path of file
+    dynamodb_table = "talent-eks-tf-lockfile"
     region = "us-east-1"
   }
 }
