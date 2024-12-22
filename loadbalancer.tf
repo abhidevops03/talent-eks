31a229e3498370fcbe6afb459edf886ce8117bfd
abhi <abhidevopspractice@gmail.com>
2024-12-22 05:57:14 +0000

    added loadbalancer.tf

---
+++ loadbalancer.tf
@@ -0,0 +1,7 @@
+module "load_balancer_controller" {
+  source = "git::https://github.com/DNXLabs/terraform-aws-eks-lb-controller.git"
+
+  cluster_identity_oidc_issuer     = module.eks.oidc_provider
+  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+  cluster_name                     = module.eks.cluster_name
+}
