5d3dc9c5a2ab030a7ee3fa6eea2552e81e306519
abhi <abhidevopspractice@gmail.com>
2024-12-22 09:12:55 +0000

    modified provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,3 @@
-
-
-
-
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
@@ -24,4 +19,3 @@ provider "helm" {
     }
   }
 }
-
